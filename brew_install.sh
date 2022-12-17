cc0423c98e0cf5b5e835b8fea10d09103219a740
Nick <90702473+codesshaman@users.noreply.github.com>
2022-12-17 18:04:10 +0300

    Update brew_install.sh

--- brew_install.sh
+++ brew_install.sh
@@ -1,6 +1,6 @@
 #!/bin/zsh
 
-mkdir homebrew && curl -L https://github.com/Homebrew/brew/tarball/master | tar xz --st$
+git clone https://github.com/Homebrew/brew homebrew
 eval "$(homebrew/bin/brew shellenv)"
 brew update --force --quiet
 chmod -R go-w "$(brew --prefix)/share/zsh"
